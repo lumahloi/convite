737faa2b2adba02f0bc1b6674a063a99281b14f0
Lumah Loise <115045300+lumahloi@users.noreply.github.com>
2023-05-13 13:42:38 -0300

    convite

--- adm.php
+++ adm.php
@@ -1,33 +1,35 @@
 <!DOCTYPE html>
-<html>
+<html lang="pt-br">
 <head>
-	<title>Visualizar arquivo CSV</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Visualizar lista de convidados</title>
 </head>
 <body>
-	<h1>Visualizar arquivo CSV</h1>
-
-	<?php
-		// abre o arquivo CSV
-		if (($csv_file = fopen("confirmados.csv", 'r')) !== FALSE) {
-
-		
-			// cria a tabela
-			echo '<table border="1">';
-			echo '<tr><th>Nome</th><th>Email</th><th>Acompanhantes</th></tr>';
-
-			// lê as linhas do arquivo CSV e exibe os dados na tabela
-			while (($data = fgetcsv($csv_file, 1000, ",")) !== FALSE) {
-				echo '<tr>';
-				foreach ($data as $cell) {
-					echo '<td>' . htmlspecialchars($cell) . '</td>';
-				}
-				echo '</tr>';
-			}
-
-			// fecha a tabela e o arquivo CSV
-			echo '</table>';
-			fclose($csv_file);
-		}
-	?>
+    <h1>Visualizar lista de convidados</h1>
 </body>
 </html>
+
+<?php
+	
+	if (($csv_file = fopen("confirmados.csv", 'r')) !== FALSE) {
+		
+	// cria a tabela
+		echo '<table border="1">';
+		echo '<tr><th>Nome</th><th>Email</th><th>Telefone</th><th colspan="3">Acompanhantes</th></tr>';
+
+		// lê as linhas do arquivo CSV e exibe os dados na tabela
+		while (($data = fgetcsv($csv_file, 1000, ",")) !== FALSE) {
+			echo '<tr>';
+			foreach ($data as $cell) {
+				echo '<td>' . htmlspecialchars($cell) . '</td>';
+			}
+			echo '</tr>';
+		}
+
+		// fecha a tabela e o arquivo CSV
+		echo '</table>';
+		fclose($csv_file);
+	}
+?>
--- confirma.php
+++ confirma.php
@@ -8,10 +8,11 @@
     $acomp3 = $_POST['acomp3'];
 
     $confirmados = array();
+
     if (($handle = fopen("confirmados.csv", "r")) !== FALSE) {
-		
         while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
             $confirmados[] = $data[1];
+            // ele pega o item[1] da variável $data- que é o email-, e acrescenta ao array confirmados
 			
 			if (in_array($email, $confirmados)) {
 				echo "Convidado já foi confirmado!";
@@ -22,7 +23,7 @@
         fclose($handle);
     }
 
-    $fp = fopen('confirmados.csv', 'a');
+    $fp = fopen('confirmados.csv', 'a'); //"a": write only
     fputcsv($fp, array($nome, $email, $telefone, $acomp1, $acomp2, $acomp3));
     fclose($fp);
 
@@ -30,4 +31,30 @@
 
     echo "Presença confirmada!";
     exit();
+
+/* 
+    fopen(): opens a file or URL. 
+        syntax: fopen(filename, mode, include_path, context)
+        example: fopen("confirmados.csv", "r");
+
+    fgetcsv():  parses a line from an open file, checking for CSV fields.
+        syntax: fgetcsv(file, length, separator, enclosure)
+        example: fgetcsv($handle, 1000, ",");
+
+    in_array(); searches an array for a specific value.
+        syntax: in_array(search, array, type)
+        example: in_array($email, $confirmados);
+
+    header(): sends a raw HTTP header to a client.
+        syntax: header(header, replace, http_response_code)
+        example: header("Refresh: 2;url=index.html");
+
+    fclose(): closes an open file pointer.
+        syntax: fclose(filepointer)
+        example: fclose($handle)
+
+    fputcsv(): formats a line as CSV and writes it to an open file.
+        syntax: fputcsv(file, fields, separator, enclosure, escape)
+        example: fputcsv($fp, array($nome, $email, $telefone, $acomp1, $acomp2, $acomp3));
+*/
 ?>
